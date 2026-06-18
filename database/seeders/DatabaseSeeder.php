2ee4101da5eca9b61c935d4dfd7b14f4da748a9e
Rafa <atmadja.1124020013@students.aknpacitan.ac.id>
2026-06-18 12:33:28 +0700

    Fix admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,18 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            AdminUserSeeder::class,
+            AdminSeeder::class,
         ]);
-        User::updateOrCreate(
-            [
-                'email' => 'admin@gmail.com',
-            ],
-            [
-                'name' => 'Administrator',
-                'password' => Hash::make('123'),
-                'role' => 'admin',
-            ]
-        );
-
     }
 }
